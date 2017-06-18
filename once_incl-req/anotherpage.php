b44c72e114b5577c3c93cd5d950175391310dac2
JuBin <jubinayoob369@gmail.com>
2017-06-19 00:43:40 +0530

    Add files via upload

---
+++ once_incl-req/anotherpage.php
@@ -0,0 +1,13 @@
+<?php
+	
+	require 'header.inc.php';
+	
+	require_once 'header.inc.php';	//if this file is required before in current page
+									//then this require_once function will not work
+	
+
+	require 'header.inc.php';
+	
+	echo $var2;
+
+?>
---
+++ once_incl-req/index.php
@@ -0,0 +1,10 @@
+<?php
+
+	include 'header.inc.php'; 
+
+	include_once 'header.inc.php'; 	//if this file is included before in current page
+									//then this include_once function will not work
+	
+	include 'header.inc.php'; 
+
+?>
